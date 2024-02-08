b62239a8c6cf4159e229de80c848623e8d64eaaa
Amit.Kumar11 <amit88sr@gmail.com>
2024-02-08 19:01:21 +0530

    COmmit square

--- Sting1.py
+++ Sting1.py
@@ -1,6 +1,14 @@
 # area=π×r^2
 
+# num = int(input("enter the number"))
+# pie = float(3.14)
+# value = pie * (num ** 2)
+# print("the area of the circle is", +value)
+# Develop a Python script that calculates the square and cube of a given number.
+
 num = int(input("enter the number"))
-pie = float(3.14)
-value = pie * (num**2)
-print("the area of the circle is", +value)
+
+value = num**3
+
+print(value)
+
